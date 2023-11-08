d12bd65f65d1fb3eded0e610ba58b645f84d2cd0
joseR0503 <JoseRamonGarridoRodr@CSESTPC16025-CA>
2023-11-08 10:04:29 +0100

    subido abecedario

---
+++ src/Abecedario.java
@@ -0,0 +1,26 @@
+
+public class Abecedario {
+
+	public static void main(String[] args) {
+		
+		
+	
+		for (int i = 97; i< 97+27-1; i++) {
+	
+			System.out.print((char) i + " ");
+	
+}
+		
+		// while 
+			
+		int contador = 97;
+		
+		while (contador < 97+27-1) {
+			
+			
+			System.out.print((char) contador + " ");
+			contador = contador +1;
+		}
+	}
+
+}
