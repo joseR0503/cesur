efd91813179522f8b512e25486193ab528c9ed95
joseR0503 <JoseRamonGarridoRodr@CSESTPC16025-CA>
2023-11-22 13:27:59 +0100

    arrays

---
+++ src/arrays.java
@@ -0,0 +1,19 @@
+
+public class arrays {
+
+	public static void main (String[] arg) {
+		
+		int[] listaInteger = new int[10];
+		 
+		
+		
+		for (int contador = 0; contador < 10; contador ++) {
+			listaInteger[contador]= contador;
+			System.out.println(listaInteger[contador]);
+			
+			
+			
+		}
+	}
+	
+}
