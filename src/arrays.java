30199f62a6c2affa3a6bcb9bfdf49d344e84e492
joseR0503 <JoseRamonGarridoRodr@CSESTPC16025-CA>
2023-11-23 12:24:10 +0100

    arrays par impar

--- src/arrays.java
+++ src/arrays.java
@@ -14,14 +14,17 @@ public class arrays {
 
 		int contadorP = 0;
 
-		int contador;
+		// int contador;
+
+		int contadorPar;
+		int contadorImpar;
 
 		System.out.print("el array inicial es : [");
 		for (int indice = 0; indice < listaInteger.length; indice++) {
 			listaInteger[indice] = aleatorio.nextInt(1000);
 			System.out.print(listaInteger[indice] + " ");
 		}
-		System.out.println( "]");
+		System.out.println("]");
 
 		for (int indice = 0; indice < listaInteger.length; indice++) {
 			if ((listaInteger[indice] % 2) == 0) {
@@ -30,32 +33,37 @@ public class arrays {
 			}
 		}
 
-		//System.out.println(contadorP);
+		// System.out.println(contadorP);
 
-		System.out.print("el array de pares es : [");
+		
 		listaIntegerPares = new int[contadorP];
-		contador = 0;
+		contadorPar = 0;
+		listaIntegerImpares = new int[listaInteger.length - contadorP];
+		contadorImpar = 0;
+
 		for (int indice = 0; indice < listaInteger.length; indice++) {
 			if ((listaInteger[indice] % 2) == 0) {
-				listaIntegerPares[contador] = listaInteger[indice];
-				System.out.print(listaIntegerPares[contador] + " ");
-				contador++;
-			}
+				listaIntegerPares[contadorPar] = listaInteger[indice];
+				contadorPar++;
+			} else {
 
+				listaIntegerImpares[contadorImpar] = listaInteger[indice];
+				contadorImpar++;
+
+			}
+		}
+		System.out.print("el array par es :[");
+		for (int indice = 0; indice < listaIntegerPares.length; indice++) {
+			System.out.print(listaIntegerPares[indice] + " ");
 		}
 		System.out.println("]");
-		
-		System.out.print("el array de impares es : [");
-		listaIntegerImpares = new int[listaInteger.length - contadorP];
-		contador = 0;
-		for (int indice = 0; indice < listaInteger.length; indice++) {
-			if ((listaInteger[indice] % 2) != 0) {
-				listaIntegerImpares[contador] = listaInteger[indice];
-				System.out.print(listaIntegerImpares[contador] + " ");
-				contador++;
-			}
-			
-		}  
-		System.out.print("]");
+
+		System.out.print("el array impar es :[ ");
+		for (int indice = 0; indice < listaIntegerImpares.length; indice++) {
+			System.out.print(listaIntegerImpares[indice] + " ");
+		}
+
+		System.out.println("]");
+
 	}
 }
