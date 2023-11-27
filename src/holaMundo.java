766bd6aae0bbda0b8e2fae7b9674be6ca8533bac
joseR0503 <JoseRamonGarridoRodr@CSESTPC16025-CA>
2023-11-27 12:09:15 +0100

    holaMundo

---
+++ src/holaMundo.java
@@ -0,0 +1,30 @@
+
+public class holaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String cadena = "hola mundo";
+		
+		for (int x=0; x < cadena.length(); x ++) {
+			
+			if (x % 2 == 0) {
+				
+				imprimir(cadena.toLowerCase(),x);
+			} else {
+				imprimir(cadena.toUpperCase(),x);
+			}
+			
+			
+		}
+		
+		cadena=cadena.toUpperCase();
+		System.out.print(cadena);
+	}
+
+	static void imprimir(String cadena , int indice) {
+		System.out.println(cadena.charAt(indice));
+	}
+	
+	
+
+}
